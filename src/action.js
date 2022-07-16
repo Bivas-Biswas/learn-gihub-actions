0b873a613def0ac4ad3459f38ac8ad73d60c33da
Bivas-Biswas <bivasbiswas1999@gmail.com>
2022-07-16 16:51:25 +0530

    :tada: add input options

--- src/action.js
+++ src/action.js
@@ -1,8 +1,13 @@
 const axios = require("axios");
+const core = require("@actions/core");
+
+const DEFAULT_CHARCTER = "dr-zoidberg";
 
 async function run() {
+  const character = core.getInput("character") || DEFAULT_CHARCTER;
+
   const res = await axios.get(
-    "https://futuramaapi.herokuapp.com/api/characters/dr-zoidberg/1"
+    `https://futuramaapi.herokuapp.com/api/characters/${character}/1`
   );
   const { data } = res;
   const firstEntry = data[0];
