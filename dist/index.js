d8dbd791cf611c2dba5609860040a70a69222b1a
Bivas-Biswas <bivasbiswas1999@gmail.com>
2022-07-16 17:10:33 +0530

    added debuged options

--- dist/index.js
+++ dist/index.js
@@ -8098,15 +8098,25 @@ const AVAILABEL_CHARCTER = ["bender", "fry", "leela", "dr-zoidberg"];
 async function run() {
   const character = core.getInput("character") || DEFAULT_CHARCTER;
 
+  core.debug(`[Futurama] Input Character: ${character}`);
+
   if (!AVAILABEL_CHARCTER.includes(character)) {
     core.setFailed(`Unknown character: ${character}`);
     return;
   }
 
+  core.debug(`[Futurama] Retrieving quote for: ${character}`);
+
   const res = await axios.get(
     `https://futuramaapi.herokuapp.com/api/characters/${character}/1`
   );
+
+  core.debug(`[Futurama] Successfully retrieved quote for: ${character}`);
+
   const { data } = res;
+
+  core.debug(`[Futurama] Data: ${JSON.stringify(data)}`);
+
   const firstEntry = data[0];
   console.log(`${firstEntry.character}: ${firstEntry.quote}`);
 
