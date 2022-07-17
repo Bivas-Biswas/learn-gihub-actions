08e948d70154f3b3c7499fe57a900745599c6f9a
Bivas-Biswas <bivasbiswas1999@gmail.com>
2022-07-17 17:18:43 +0530

    added api call

--- src/action.js
+++ src/action.js
@@ -3,9 +3,8 @@ const core = require("@actions/core");
 
 const DEFAULT_CHARCTER = "dr-zoidberg";
 
-const AVAILABEL_CHARCTER = ["bender", "fry", "leela", "dr-zoidberg"];
+// const AVAILABEL_CHARCTER = ["bender", "fry", "leela", "dr-zoidberg"];
 
-const testFolder = "/";
 const fs = require("fs");
 const path = require("path");
 
@@ -15,37 +14,45 @@ async function run() {
   fs.readFile(packagePath, (err, data) => {
     if (err) throw err;
     let student = JSON.parse(data);
-    core.setOutput(student);
-  });
-  fs.readdir(process.cwd(), (err, files) => {
-    files.forEach((file, idx) => {
-      core.setOutput(`file${idx + 1}`, file);
-    });
+
+    axios
+      .post("https://pz4k9u-3000.sse.codesandbox.io/api/users", {
+        user: { ...student },
+      })
+      .then((res) => {
+        console.log(`Status: ${res.status}`);
+        console.log("Body: ", res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+
+    // core.setOutput(JSON.stringify(student));
   });
   //
   // core.debug(`[Futurama] Input Character: ${character}`);
 
-  if (!AVAILABEL_CHARCTER.includes(character)) {
-    core.setFailed(`Unknown character: ${character}`);
-    return;
-  }
-
-  // core.debug(`[Futurama] Retrieving quote for: ${character}`);
-
-  const res = await axios.get(
-    `https://futuramaapi.herokuapp.com/api/characters/${character}/1`
-  );
-
-  // core.debug(`[Futurama] Successfully retrieved quote for: ${character}`);
-
-  const { data } = res;
-
-  // core.debug(`[Futurama] Data: ${JSON.stringify(data)}`);
-
-  const firstEntry = data[0];
-  console.log(`${firstEntry.character}: ${firstEntry.quote}`);
-
-  core.setOutput("quote", firstEntry);
+  // if (!AVAILABEL_CHARCTER.includes(character)) {
+  //   core.setFailed(`Unknown character: ${character}`);
+  //   return;
+  // }
+  //
+  // // core.debug(`[Futurama] Retrieving quote for: ${character}`);
+  //
+  // const res = await axios.get(
+  //   `https://futuramaapi.herokuapp.com/api/characters/${character}/1`
+  // );
+  //
+  // // core.debug(`[Futurama] Successfully retrieved quote for: ${character}`);
+  //
+  // const { data } = res;
+  //
+  // // core.debug(`[Futurama] Data: ${JSON.stringify(data)}`);
+  //
+  // const firstEntry = data[0];
+  // console.log(`${firstEntry.character}: ${firstEntry.quote}`);
+  //
+  // core.setOutput("quote", firstEntry);
 }
 
 run();
