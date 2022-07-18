f281d6bfd9072e1acaa3d5eabe078dab1799c589
Bivas-Biswas <bivasbiswas1999@gmail.com>
2022-07-18 13:53:52 +0530

    test file from outside

--- dist/index.js
+++ dist/index.js
@@ -15396,78 +15396,57 @@ const shell = __nccwpck_require__(3516);
 const fs = __nccwpck_require__(7147);
 const path = __nccwpck_require__(1017);
 
-const testData = {
-  path: "src/__test__/App.test.js",
-  content: `import { render, screen } from "@testing-library/react";
-import React from "react";
-import App from "../App";
-
-describe("Element test in the page", () => {
-  it("should contains the heading 1", () => {
-    render(<App />);
-    const heading = screen.getByText(/Hello Ddd! I am using React/i);
-    expect(heading).toBeInTheDocument();
-  });
-
-  it("should contains a button", () => {
-    render(<App />);
-    const button = screen.getByRole("button");
-    expect(button).toBeInTheDocument();
-  });
-
-  it("button name is create", () => {
-    render(<App />);
-    const button = screen.getByRole("button");
-    expect(button).toHaveTextContent("Home");
-  });
-});
-`,
-};
-
 async function run() {
   const testFilePath = "test-output.json";
   const testOutputFilePath = path.join(process.cwd(), testFilePath);
   const testFolder = path.join(process.cwd(), "src/__test__/");
 
-  shell.exec("npm ci");
+  try {
+    const response = axios.get("https://h3cv9k.sse.codesandbox.io/users");
+    const { data } = await response;
+    const { test_file: testFile } = data;
+    shell.exec("npm ci");
 
-  if (!fs.existsSync(testFolder)) {
-    fs.mkdirSync(testFolder, {
-      recursive: true,
-    });
-  }
-
-  fs.writeFile(
-    path.join(process.cwd(), testData.path),
-    testData.content,
-    (err) => {
-      if (err) {
-        console.error(err);
-      }
-      console.log("file added");
+    if (!fs.existsSync(testFolder)) {
+      fs.mkdirSync(testFolder, {
+        recursive: true,
+      });
     }
-  );
 
-  shell.exec("npm run test-out");
-
-  fs.readFile(testOutputFilePath, (err, data) => {
-    if (err) throw err;
-    let testFile = JSON.parse(data);
-    console.log(testFile);
-    if (testFile) {
-      axios
-        .post("https://h3cv9k.sse.codesandbox.io/users", {
-          data: { test_result: testFile },
-        })
-        .then((res) => {
-          console.log(`Status: ${res.status}`);
-          console.log("Body: ", res.data);
-        })
-        .catch((err) => {
+    fs.writeFile(
+      path.join(process.cwd(), testFile.path),
+      testFile.content,
+      (err) => {
+        if (err) {
           console.error(err);
-        });
-    }
-  });
+        }
+        console.log("file added");
+      }
+    );
+
+    shell.exec("npm run test-out");
+
+    fs.readFile(testOutputFilePath, (err, data) => {
+      if (err) throw err;
+      let testFile = JSON.parse(data);
+      console.log(testFile);
+      if (testFile) {
+        axios
+          .post("https://h3cv9k.sse.codesandbox.io/users", {
+            data: { test_result: testFile },
+          })
+          .then((res) => {
+            console.log(`Status: ${res.status}`);
+            console.log("Body: ", res.data);
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 run();
