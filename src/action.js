9e5d590c785ed550215d6381a4ef4e3fe14f3aa8
Bivas-Biswas <bivasbiswas1999@gmail.com>
2022-07-19 11:29:41 +0530

    added inputs with api

--- src/action.js
+++ src/action.js
@@ -10,55 +10,67 @@ async function run() {
   const testOutputFilePath = path.join(process.cwd(), testFilePath);
   const testFolder = path.join(process.cwd(), "src/__test__/");
 
-  const userId = core.getInput("user_id") || "";
-  const thaNo = core.getInput("tha_no") || "";
-  console.log(userId, thaNo);
-  // try {
-  //   const response = axios.get("https://h3cv9k.sse.codesandbox.io/users");
-  //   const { data } = await response;
-  //   const { test_file: testFile } = data;
-  //   shell.exec("npm ci");
-  //
-  //   if (!fs.existsSync(testFolder)) {
-  //     fs.mkdirSync(testFolder, {
-  //       recursive: true,
-  //     });
-  //   }
-  //
-  //   fs.writeFileSync(
-  //     path.join(process.cwd(), testFile.path),
-  //     testFile.content,
-  //     (err) => {
-  //       if (err) {
-  //         console.error(err);
-  //       }
-  //       console.log("file added");
-  //     }
-  //   );
-  //
-  //   shell.exec("npm run test-out");
-  //
-  //   fs.readFile(testOutputFilePath, (err, data) => {
-  //     if (err) throw err;
-  //     let testFile = JSON.parse(data);
-  //     console.log(testFile);
-  //     if (testFile) {
-  //       axios
-  //         .post("https://h3cv9k.sse.codesandbox.io/users", {
-  //           data: { test_result: testFile },
-  //         })
-  //         .then((res) => {
-  //           console.log(`Status: ${res.status}`);
-  //           console.log("Body: ", res.data);
-  //         })
-  //         .catch((err) => {
-  //           console.error(err);
-  //         });
-  //     }
-  //   });
-  // } catch (err) {
-  //   console.log(err);
-  // }
+  const user_id = core.getInput("user_id") || "";
+  const tha_no = core.getInput("tha_no") || "";
+
+  const curr_dir_length = fs.readdirSync(path.join(process.cwd(), "/")).length;
+
+  if (
+    fs.existsSync(path.join(process.cwd(), "package.json")) ||
+    curr_dir_length < 3
+  ) {
+    try {
+      const response = axios.get(
+        `https://h3cv9k.sse.codesandbox.io/users?user_id=${user_id}&tha_no=${tha_no}`
+      );
+      const { data } = await response;
+      const { test_file: testFile } = data;
+
+      shell.exec("npm ci");
+
+      if (!fs.existsSync(testFolder)) {
+        fs.mkdirSync(testFolder, {
+          recursive: true,
+        });
+      }
+
+      fs.writeFileSync(
+        path.join(process.cwd(), testFile.path),
+        testFile.content,
+        (err) => {
+          if (err) {
+            console.error(err);
+          }
+          console.log("file added");
+        }
+      );
+
+      shell.exec("npm run test-out");
+
+      fs.readFile(testOutputFilePath, (err, data) => {
+        if (err) throw err;
+        let testFile = JSON.parse(data);
+        console.log(testFile);
+        if (testFile) {
+          axios
+            .post("https://h3cv9k.sse.codesandbox.io/users", {
+              data: { test_result: testFile, userDetails: { user_id, tha_no } },
+            })
+            .then((res) => {
+              console.log(`Status: ${res.status}`);
+              console.log("Body: ", res.data);
+            })
+            .catch((err) => {
+              console.error(err);
+            });
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  } else {
+    console.log("package.json not detect");
+  }
 }
 
 run();
