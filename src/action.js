be01dbb525097dbddb52b29da39e71482da45064
Bivas-Biswas <bivasbiswas1999@gmail.com>
2022-07-20 08:16:02 +0530

    test secrets

--- src/action.js
+++ src/action.js
@@ -10,77 +10,63 @@ async function run() {
   const testOutputFilePath = path.join(process.cwd(), testFilePath);
   const testFolder = path.join(process.cwd(), "src/__test__/");
 
-  const user_id = core.getInput("user_id") || "fuck";
-  const tha_no = core.getInput("tha_no") || "fuck";
+  const user_id = core.getInput("user_id") || "no user id";
+  const tha_no = core.getInput("tha_no") || "no tha no";
 
-  const curr_dir_length = fs.readdirSync(path.join(process.cwd(), "/")).length;
+  if (fs.existsSync(path.join(process.cwd(), "Day01"))) {
+    try {
+      const response = axios.get(
+        `https://h3cv9k.sse.codesandbox.io/users?user_id=${user_id}&tha_no=${tha_no}`
+      );
+      const { data } = await response;
+      const { test_file: testFile } = data;
 
-  console.log(user_id, tha_no);
-  axios
-    .post("https://h3cv9k.sse.codesandbox.io/users", {
-      data: { userDetails: { user_id, tha_no } },
-    })
-    .then((res) => {
-      console.log(`Status: ${res.status}`);
-      console.log("Body: ", res.data);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+      shell.exec(`cd Day${tha_no}`);
+      shell.exec("npm ci");
 
-  // if (curr_dir_length < 3) {
-  //   try {
-  //     const response = axios.get(
-  //       `https://h3cv9k.sse.codesandbox.io/users?user_id=${user_id}&tha_no=${tha_no}`
-  //     );
-  //     const { data } = await response;
-  //     const { test_file: testFile } = data;
-  //
-  //     shell.exec("npm ci");
-  //
-  //     if (!fs.existsSync(testFolder)) {
-  //       fs.mkdirSync(testFolder, {
-  //         recursive: true,
-  //       });
-  //     }
-  //
-  //     fs.writeFileSync(
-  //       path.join(process.cwd(), testFile.path),
-  //       testFile.content,
-  //       (err) => {
-  //         if (err) {
-  //           console.error(err);
-  //         }
-  //         console.log("file added");
-  //       }
-  //     );
-  //
-  //     shell.exec("npm run test-out");
-  //
-  //     fs.readFile(testOutputFilePath, (err, data) => {
-  //       if (err) throw err;
-  //       let testFile = JSON.parse(data);
-  //       console.log(testFile);
-  //       if (testFile) {
-  //         axios
-  //           .post("https://h3cv9k.sse.codesandbox.io/users", {
-  //             data: { test_result: testFile, userDetails: { user_id, tha_no } },
-  //           })
-  //           .then((res) => {
-  //             console.log(`Status: ${res.status}`);
-  //             console.log("Body: ", res.data);
-  //           })
-  //           .catch((err) => {
-  //             console.error(err);
-  //           });
-  //       }
-  //     });
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // } else {
-  //   console.log("package.json not detect");
-  // }
+      if (!fs.existsSync(testFolder)) {
+        fs.mkdirSync(testFolder, {
+          recursive: true,
+        });
+      }
+
+      fs.writeFileSync(
+        path.join(process.cwd(), testFile.path),
+        testFile.content,
+        (err) => {
+          if (err) {
+            console.error(err);
+          }
+          console.log("file added");
+        }
+      );
+
+      shell.exec("npm run test-out");
+
+      fs.readFile(testOutputFilePath, (err, data) => {
+        if (err) throw err;
+        let testFile = JSON.parse(data);
+        console.log(testFile);
+        if (testFile) {
+          axios
+            .post("https://h3cv9k.sse.codesandbox.io/users", {
+              data: { test_result: testFile, userDetails: { user_id, tha_no } },
+            })
+            .then((res) => {
+              console.log(`Status: ${res.status}`);
+              console.log("Body: ", res.data);
+            })
+            .catch((err) => {
+              console.error(err);
+            });
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  } else {
+    console.log("Day1 folder not added yet");
+  }
 }
 
 run();
